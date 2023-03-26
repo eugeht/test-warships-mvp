0040d0e75a58eb561ecee1c66c55c7cda34a86e9
Eugene N <eugennizamutdinov@gmail.com>
2023-03-26 09:15:43 +0200

    chore: developing

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,5 +10,9 @@ export type ApiResponse<T> = AxiosResponse<T>
 
 // Api client
 export const api = axios.create( {
-  baseURL: 'https://vortex.worldofwarships.eu/api/encyclopedia/en/',
+  baseURL : 'https://vortex.worldofwarships.eu/api/encyclopedia/en/',
+  headers : {
+    'Access-Control-Allow-Origin'  : '*',
+    'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  },
 } )
